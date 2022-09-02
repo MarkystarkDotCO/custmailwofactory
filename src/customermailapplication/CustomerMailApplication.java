912073f46e28745ea55e27af2789fb91a3534a14
Methanon Keaokrachang <48640302+MarkystarkDotCO@users.noreply.github.com>
2022-09-02 09:26:00 +0700

    Add files via upload

--- src/customermailapplication/CustomerMailApplication.java
+++ src/customermailapplication/CustomerMailApplication.java
@@ -1,35 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 package customermailapplication;
 
 import java.util.Scanner;
 
-
+/**
+ *
+ * @author sarun
+ */
 public class CustomerMailApplication {
 
     /**
      * @param args the command line arguments
      */
     private static Customer customer;
-   
+    public void getCustomerTypeFromUser(String customerType) {
+        switch(customerType) {
+            case "Regular":
+                customer = CustomerFactory.createCustomer("Regular");
+                break;
+            case "Mountain":
+                customer = CustomerFactory.createCustomer("Mountain");
+                break;
+            case "Delinquent":
+                customer = CustomerFactory.createCustomer("Delinquent");
+                break;
+        }
+    }
+    public String generateMail() {
+        return customer.createMail();
+    }
+    
     public static void main(String[] args) {
-        CustomerFactory app2 = new CustomerFactory();
+        CustomerMailApplication app = new CustomerMailApplication();
         Scanner inp = new Scanner(System.in);
         System.out.print("Please choose customer type 1. Regular, 2. Mountain, 3. Delinquent ");
         int type = inp.nextInt();
         switch(type) {
             case 1:
-                //app.getCustomerTypeFromUser("Regular");
-                customer = app2.createCustomer("Regular");
+                app.getCustomerTypeFromUser("Regular");
                 break;
             case 2:
-                //app.getCustomerTypeFromUser("Mountain");
-                customer = app2.createCustomer("Mountain");
+                app.getCustomerTypeFromUser("Mountain");
                 break;
             case 3:
-                //app.getCustomerTypeFromUser("Delinquent");
-                customer = app2.createCustomer("Delinquent");
+                app.getCustomerTypeFromUser("Delinquent");
                 break;
             
         }
-        System.out.println(customer.createMail());        
+        System.out.println(customer.createMail());          
     }
 }
